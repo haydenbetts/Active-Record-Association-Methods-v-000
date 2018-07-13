0ec1048575e55259a95ead1e9519d67a88a526de
Hayden Betts <haydenbetts@gmail.com>
2018-07-13 17:18:33 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -11,14 +11,13 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    #return the number of genres associated with the artist
-    # inject
     self.songs.inject(0) do |sum, song|
       if song.genre
         sum += 1
       else
         sum
       end
+      
     end
 
   end
