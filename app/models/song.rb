614cf0510380ca6dddf1f29c40e1e5a1da118007
Hayden Betts <haydenbetts@gmail.com>
2018-07-13 17:29:04 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,7 +10,7 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     binding.pry
-    self.artist = Artist.find_by(name: "Drake")
+    self.artist = Artist.new(name: "Drake")
     self.artist
   end
 end
